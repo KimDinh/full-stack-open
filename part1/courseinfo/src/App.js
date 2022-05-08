275db118cb986f6b8b6ffec282eb0bc637ee2cf1
KimDinh <hoangkimdinh1608@gmail.com>
2022-05-08 02:55:59 -0700

    update .gitignore, complete courseinfo 1.1-1.5

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,48 @@
+const Header = (props) => (
+  <h1>{props.course}</h1>
+)
+const Part = (props) => (
+  <p>{props.part} {props.exercise}</p>
+)
+
+const Content = (props) => (
+  <>
+    <Part part={props.parts[0].name} exercise={props.parts[0].exercises} />
+    <Part part={props.parts[1].name} exercise={props.parts[1].exercises} />
+    <Part part={props.parts[2].name} exercise={props.parts[2].exercises} />
+  </>
+)
+
+const Total = (props) => (
+  <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
+)
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+export default App
