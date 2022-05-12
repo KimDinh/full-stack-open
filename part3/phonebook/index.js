928b4f2b206b4de0ef683ee4a258ff5368bec4c2
KimDinh <hoangkimdinh1608@gmail.com>
2022-05-11 20:01:41 -0700

    complete phonebook backend 3.1-3.8

---
+++ part3/phonebook/index.js
@@ -0,0 +1,100 @@
+const { request, response } = require('express')
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+
+app.use(express.json())
+
+morgan.token('post-body', (request, response) =>
+  request.method === 'POST' ? JSON.stringify(request.body) : ""  
+)
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :post-body")
+)
+
+let persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+]
+
+app.get("/api/persons", (request, response) => {
+  response.json(persons)
+})
+
+app.get("/info", (request, response) => {
+  response.send(`
+    <p>Phonebook has info for ${persons.length} people</p>
+    <p>${new Date()}</p>
+  `)
+})
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+  
+  response.status(204).end()
+})
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body
+  
+  if (!body.name) {
+    return response.status(400).json({
+      error: "name missing"
+    })
+  }
+  
+  if (!body.number) {
+    return response.status(400).json({
+      error: "number missing"
+    })
+  }
+
+  if (persons.some(person => person.name === body.name)) {
+    return response.status(400).json({
+      error: "name already exists"
+    })
+  }
+
+  const person = { 
+    name: body.name,
+    number: body.number,
+    id: Math.floor(Math.random()*10000)
+  }
+  persons.concat(person)
+  response.json(person)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+})
