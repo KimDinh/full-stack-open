f8163a33010933402fc85063f4da806e61df3605
Kim Dinh <33998323+KimDinh@users.noreply.github.com>
2026-02-11 18:39:35 +0700

    Completed 4.1-4.2: refactor bloglist app

---
+++ part4/bloglist/app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
+const blogsRouter = require('./controllers/blogs')
+
+const app = express()
+
+mongoose
+  .connect(config.MONGODB_URI, { family: 4 })
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connection to MongoDB:', error.message)
+  })
+
+app.use(express.json())
+app.use(middleware.requestLogger)
+app.use('/api/blogs', blogsRouter)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
